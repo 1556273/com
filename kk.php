1911cd6ab14cab5ad3b87f08d1d5b0534a4ea35e
1556273 <ezugwukamsiyochukwupeniel@gmail.com>
2024-01-26 11:57:36 -0500

    code changes

---
+++ kk.php
@@ -0,0 +1,11 @@
+    <?php
+    //string
+    $name = 'kamsi';
+    //integer
+    $name = "45";
+    //float
+    $name = "45.99";
+    //array
+    $names = array("danial","tom");
+    echo $names['0'];
+    ?>
